0efad8136cc46ff87d1b271e8d1f4eeb5163d27e
Renato M. Santana <120746158+renatomsantana@users.noreply.github.com>
2024-12-11 12:15:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    # Usa a variável de ambiente 'PORT' ou a porta 5000 por padrão
+    port = int(os.environ.get('PORT', 5000))  # Pegando a variável 'PORT' ou usando 5000 se não existir
+    app.run(host='0.0.0.0', port=port)
